71d68a63b454793043fb0e8947bd0d4993298afb
Anselmo <afajunior@gmail.com>
2020-05-02 22:43:29 -0300

    Utilizando um Form para composição de mensagens. Adicionado validador de e-mail e um no corpo da mensagem.

--- lib/MessageCompose.dart
+++ lib/MessageCompose.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:emailapp/Message.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,8 @@ class _MessageComposeState extends State<MessageCompose> {
   String subject = "";
   String body = "";
 
+  final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,47 +21,58 @@ class _MessageComposeState extends State<MessageCompose> {
           title: Text('Compose New Message'),
         ),
         body: SingleChildScrollView(
-            child: Column(
-          children: <Widget>[
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  to = value;
+            child: Form(
+          key: key,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: TextFormField(
+                  validator: (value) => !EmailValidator.validate(value)
+                      ? "`TO` field must be a valid email"
+                      : null,
+                  onSaved: (value) => to = value,
+                  decoration: InputDecoration(
+                      labelText: 'TO',
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+              ListTile(
+                  title: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "`SUBJECT` cannot be empty";
+                  } else if (value.length < 4) {
+                    return "`SUBJECT` must be longer than 4 characters";
+                  }
+                  return null;
                 },
+                onSaved: (value) => subject = value,
                 decoration: InputDecoration(
-                    labelText: 'TO',
+                    labelText: 'SUBJECT',
                     labelStyle: TextStyle(fontWeight: FontWeight.bold)),
-              ),
-            ),
-            ListTile(
-                title: TextField(
-              onChanged: (value) {
-                subject = value;
-              },
-              decoration: InputDecoration(
-                  labelText: 'SUBJECT',
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold)),
-            )),
-            ListTile(
-                title: TextField(
-              onChanged: (value) {
-                body = value;
-              },
-              decoration: InputDecoration(
-                  labelText: 'BODY',
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold)),
-              maxLines: 9,
-            )),
-            ListTile(
-              title: RaisedButton(
-                child: Text('SEND'),
-                onPressed: () {
-                  Message message = Message(subject, body);
-                  Navigator.pop(context, message);
-                },
-              ),
-            )
-          ],
+              )),
+              ListTile(
+                  title: TextFormField(
+                onSaved: (value) => body = value,
+                decoration: InputDecoration(
+                    labelText: 'BODY',
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                maxLines: 9,
+              )),
+              ListTile(
+                title: RaisedButton(
+                  child: Text('SEND'),
+                  onPressed: () {
+                    if (this.key.currentState.validate()) {
+                      this.key.currentState.save();
+                      Message message = Message(subject, body);
+                      Navigator.pop(context, message);
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
         )));
   }
 
